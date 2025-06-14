34f558b1635f8150b73b65bf5e438923dc00d6eb
dmfshove <funnymoments610@gmail.com>
2025-06-14 21:08:06 +0200

    styles(grow): write SCSS styles for GrowSection component

--- frontend/src/components/GrowSection/GrowSection.tsx
+++ frontend/src/components/GrowSection/GrowSection.tsx
@@ -21,7 +21,7 @@ function GrowSection() {
                     </div>
                 </div>
                 <div className="grow__row grow__row--2 row">
-                    <div className="grow__futures-wrap">
+                    <div className="grow__futures-wrap grow__grid">
                         <div className="grow__feature grow__grid-column grow__grid-column--a">
                             <img className="grow__feature-icon" src={DairyImage} alt="Dairy Products" width={52} height={55} />
 
@@ -52,7 +52,7 @@ function GrowSection() {
 
 
                         <div className="grow__grid-column grow__grid-column--g">
-                            <img src={NutsPackageImage} alt="Nuts Package" className="grow__image" />
+                            <img src={NutsPackageImage} alt="Nuts Package" className="grow__image" width={743} height={990} />
                         </div>
 
 
@@ -60,7 +60,7 @@ function GrowSection() {
 
 
 
-                        <div className="grow__feature grow__grid-column grow__grid-column--a">
+                        <div className="grow__feature grow__grid-column grow__grid-column--d">
                             <img className="grow__feature-icon" src={AgriculturalImage} alt="Agricultural Services" width={61} height={62} />
 
                             <h6 className="grow__feature-title h6">Agricultural Services</h6>
@@ -68,7 +68,7 @@ function GrowSection() {
                             <p className="grow__feature-description text">Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
                         </div>
 
-                        <div className="grow__feature grow__grid-column grow__grid-column--b">
+                        <div className="grow__feature grow__grid-column grow__grid-column--e">
                             <img className="grow__feature-icon" src={OrganicProductsImage} alt="Organic Products" width={58} height={56} />
 
                             <h6 className="grow__feature-title h6">Organic Products</h6>
@@ -76,7 +76,7 @@ function GrowSection() {
                             <p className="grow__feature-description text">Sed ut perspiciatis unde omnis iste natus error sit voluptat accusantium doloremqlaudantium. Sed ut perspiciatis</p>
                         </div>
 
-                        <div className="grow__feature grow__grid-column grow__grid-column--c">
+                        <div className="grow__feature grow__grid-column grow__grid-column--f">
                             <img className="grow__feature-icon" src={FreshVegetablesImage} alt="Fresh Vegetables" width={61} height={62} />
 
                             <h6 className="grow__feature-title h6">Fresh Vegetables</h6>
@@ -86,14 +86,15 @@ function GrowSection() {
                     </div>
                 </div>
                 <div className="grow__row grow__row--3 row">
-                    <div className="grow__column grow__column--2"></div>
-                    <button className="grow__button button button--transparent">
-                        Explore More
-                        <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="9.5" cy="9.5" r="9.5" fill="#335B6B" />
-                            <path className="button__svg-arrow" d="M9.4769 6.12891L12.8714 9.19342L9.4769 12.2579M12.4 9.19342H5.5166" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </button>
+                    <div className="grow__column grow__column--2">
+                        <button className="grow__button button button--transparent">
+                            Explore More
+                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="9.5" cy="9.5" r="9.5" fill="#335B6B" />
+                                <path className="button__svg-arrow" d="M9.4769 6.12891L12.8714 9.19342L9.4769 12.2579M12.4 9.19342H5.5166" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>
