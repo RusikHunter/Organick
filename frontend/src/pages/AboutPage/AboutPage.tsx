7f0253b6bdc6c31550f85c7c5ae1f5c27bc90d1a
dmfshove <funnymoments610@gmail.com>
2025-06-14 14:44:03 +0200

    feat(creativesection): write HTML markup for CreativeSection component

--- frontend/src/pages/AboutPage/AboutPage.tsx
+++ frontend/src/pages/AboutPage/AboutPage.tsx
@@ -1,10 +1,12 @@
 import SubIntroBlock from "../../components/SubIntroBlock/SubIntroBlock"
 import AboutUsBackgroundImage from "../../images/images/background-about-us.png"
+import CreativeSection from "../../components/CreativeSection/CreativeSection"
 
 function AboutPage() {
     return (
         <>
             <SubIntroBlock title="About Us" backgroundImageURL={AboutUsBackgroundImage} />
+            <CreativeSection />
         </>
     )
 }
