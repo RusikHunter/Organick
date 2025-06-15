d0d4a21dcda985cbf3d5ebcf676c9af8b879c932
dmfshove <funnymoments610@gmail.com>
2025-06-15 14:16:44 +0200

    styles(video): write adaptive SCSS styles for VideoSection component

--- frontend/src/components/VideoSection/VideoSection.tsx
+++ frontend/src/components/VideoSection/VideoSection.tsx
@@ -9,17 +9,19 @@ function VideoSection() {
                     <div className="video__column column">
                         <video src={FruitsMovie} className="video__movie"></video>
 
-                        <span className="video__promo promo">Organic Only</span>
+                        <div className="video__content-wrap">
+                            <span className="video__promo promo">Organic Only</span>
 
-                        <h2 className="video__title h2">Everyday Fresh & Clean</h2>
+                            <h2 className="video__title h2">Everyday Fresh & Clean</h2>
 
-                        <p className="video__description text">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the </p>
+                            <p className="video__description text">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the </p>
 
-                        <button className="video__button">
-                            <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20.5 9.90201C22.5 11.0567 22.5 13.9435 20.5 15.0982L4.75 24.1914C2.75 25.3461 0.25 23.9028 0.25 21.5934L0.250001 3.40682C0.250001 1.09741 2.75 -0.345957 4.75 0.808744L20.5 9.90201Z" fill="white" />
-                            </svg>
-                        </button>
+                            <button className="video__button">
+                                <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M20.5 9.90201C22.5 11.0567 22.5 13.9435 20.5 15.0982L4.75 24.1914C2.75 25.3461 0.25 23.9028 0.25 21.5934L0.250001 3.40682C0.250001 1.09741 2.75 -0.345957 4.75 0.808744L20.5 9.90201Z" fill="white" />
+                                </svg>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
