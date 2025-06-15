1e016064a8b477bbad75dee61b60f4f99632dbea
dmfshove <funnymoments610@gmail.com>
2025-06-15 17:54:56 +0200

    feat(portfoliosection): write HTML markup for PortfolioSection component

--- frontend/src/components/PortfolioSection/PortfolioSection.tsx
+++ frontend/src/components/PortfolioSection/PortfolioSection.tsx
@@ -2,7 +2,17 @@ import "./PortfolioSection.scss"
 
 function PortfolioSection() {
     return (
-        <section className="portfolio"></section>
+        <section className="portfolio">
+            <div className="portfolio__inner container">
+                <div className="portfolio__row portfolio__row--1 row">
+
+                </div>
+
+                <div className="portfolio__row portfolio__row--2 row">
+
+                </div>
+            </div>
+        </section>
     )
 }
 
