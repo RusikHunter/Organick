b89883b214ba316bf9b484ec7b2ee1f73f347273
dmfshove <funnymoments610@gmail.com>
2025-06-17 16:59:17 +0200

    feat(products): write SCSS styles for ProductsWrap and ProductCard components

--- frontend/src/components/ProductsWrap/ProductsWrap.tsx
+++ frontend/src/components/ProductsWrap/ProductsWrap.tsx
@@ -1,9 +1,13 @@
 import "./ProductsWrap.scss"
+import ProductCard from "../ProductCard/ProductCard"
 
 function ProductsWrap() {
     return (
         <article className="products-wrap">
-
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </article>
     )
 }
