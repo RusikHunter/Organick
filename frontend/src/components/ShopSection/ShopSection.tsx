ed9d9c2a6f8817aa2ca0c8fefb3a5cac039ccffd
dmfshove <funnymoments610@gmail.com>
2025-06-17 16:14:34 +0200

    feat(shop): write HTML markup for ShopSection component

---
+++ frontend/src/components/ShopSection/ShopSection.tsx
@@ -0,0 +1,17 @@
+import "./ShopSection.scss"
+
+function ShopSection() {
+    return (
+        <section className="shop">
+            <div className="shop__inner container">
+                <div className="shop__row row">
+                    <div className="shop__column column">
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ShopSection
--- frontend/src/pages/ShopPage/ShopPage.tsx
+++ frontend/src/pages/ShopPage/ShopPage.tsx
@@ -1,11 +1,15 @@
 import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
+import SubIntroBlock from "../../components/SubIntroBlock/SubIntroBlock"
+import ShopBackgroundImage from "../../images/images/background-shop.png"
+import ShopSection from "../../components/ShopSection/ShopSection"
 
 function ShopPage() {
     useSetCurrentPage("shop")
 
     return (
         <>
-            <h1>ShopPage</h1>
+            <SubIntroBlock title="Shop" backgroundImageURL={ShopBackgroundImage} />
+            <ShopSection />
         </>
     )
 }
