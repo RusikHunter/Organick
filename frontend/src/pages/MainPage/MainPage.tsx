c4a45047f09196ac53889f52edb71c8e4772dc3c
dmfshove <funnymoments610@gmail.com>
2025-06-17 18:54:16 +0200

    styles(organic): write adaptive SCSS styles for OrganicSection component

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -24,7 +24,7 @@ function MainPage() {
             <OrganicSection />
             <EcoSection />
             <CategoriesListSection />
-            <MoreNewsSection />
+            {/* <MoreNewsSection /> */}
         </>
     )
 }
