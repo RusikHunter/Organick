849688ec22c609810f5c5bde5bcaa8d8faaa396e
dmfshove <funnymoments610@gmail.com>
2025-06-18 16:22:02 +0200

    feat(productitem): write HTML markup for ProductItemSection component

---
+++ frontend/src/components/ProductItemSection/ProductItemSection.tsx
@@ -0,0 +1,84 @@
+import "./ProductItemSection.scss"
+import TEMPNUTS from "../../images/images/TEMP-NUTS.png"
+
+function ProductItemSection() {
+    return (
+        <section className="product-item">
+            <div className="product-item__inner container">
+                <div className="product-item__row product-item__row--1 row">
+                    <div className="product-item__column product-item__column--1 column">
+                        <span className="product-item__category">Millets</span>
+
+                        <img src={TEMPNUTS} alt="Nuts" className="product-item__image" width={591} height={563} />
+                    </div>
+
+                    <div className="product-item__column product-item__column--2 column">
+                        <h3 className="product-item__title h3"></h3>
+
+                        <div className="product-item__rate">
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.67806 1.09367C6.98566 0.189974 8.26378 0.189977 8.57138 1.09367L9.5549 3.98313C9.69285 4.3884 10.0735 4.66091 10.5016 4.66091H13.5818C14.5617 4.66091 14.9569 5.92416 14.1517 6.48263L11.7409 8.15456C11.3741 8.40901 11.2203 8.87584 11.3642 9.29851L12.3036 12.0586C12.6145 12.9718 11.5798 13.7523 10.7871 13.2025L8.19462 11.4046C7.85187 11.1668 7.39758 11.1668 7.05483 11.4046L4.46235 13.2025C3.66969 13.7523 2.63496 12.9718 2.9458 12.0586L3.88528 9.29851C4.02915 8.87584 3.87539 8.40901 3.50851 8.15456L1.09777 6.48263C0.292535 5.92416 0.687722 4.66091 1.66767 4.66091H4.74788C5.17598 4.66091 5.55659 4.3884 5.69454 3.98313L6.67806 1.09367Z" fill="#FFA858" />
+                            </svg>
+
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.67806 1.09367C6.98566 0.189974 8.26378 0.189977 8.57138 1.09367L9.5549 3.98313C9.69285 4.3884 10.0735 4.66091 10.5016 4.66091H13.5818C14.5617 4.66091 14.9569 5.92416 14.1517 6.48263L11.7409 8.15456C11.3741 8.40901 11.2203 8.87584 11.3642 9.29851L12.3036 12.0586C12.6145 12.9718 11.5798 13.7523 10.7871 13.2025L8.19462 11.4046C7.85187 11.1668 7.39758 11.1668 7.05483 11.4046L4.46235 13.2025C3.66969 13.7523 2.63496 12.9718 2.9458 12.0586L3.88528 9.29851C4.02915 8.87584 3.87539 8.40901 3.50851 8.15456L1.09777 6.48263C0.292535 5.92416 0.687722 4.66091 1.66767 4.66091H4.74788C5.17598 4.66091 5.55659 4.3884 5.69454 3.98313L6.67806 1.09367Z" fill="#FFA858" />
+                            </svg>
+
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.67806 1.09367C6.98566 0.189974 8.26378 0.189977 8.57138 1.09367L9.5549 3.98313C9.69285 4.3884 10.0735 4.66091 10.5016 4.66091H13.5818C14.5617 4.66091 14.9569 5.92416 14.1517 6.48263L11.7409 8.15456C11.3741 8.40901 11.2203 8.87584 11.3642 9.29851L12.3036 12.0586C12.6145 12.9718 11.5798 13.7523 10.7871 13.2025L8.19462 11.4046C7.85187 11.1668 7.39758 11.1668 7.05483 11.4046L4.46235 13.2025C3.66969 13.7523 2.63496 12.9718 2.9458 12.0586L3.88528 9.29851C4.02915 8.87584 3.87539 8.40901 3.50851 8.15456L1.09777 6.48263C0.292535 5.92416 0.687722 4.66091 1.66767 4.66091H4.74788C5.17598 4.66091 5.55659 4.3884 5.69454 3.98313L6.67806 1.09367Z" fill="#FFA858" />
+                            </svg>
+
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.67806 1.09367C6.98566 0.189974 8.26378 0.189977 8.57138 1.09367L9.5549 3.98313C9.69285 4.3884 10.0735 4.66091 10.5016 4.66091H13.5818C14.5617 4.66091 14.9569 5.92416 14.1517 6.48263L11.7409 8.15456C11.3741 8.40901 11.2203 8.87584 11.3642 9.29851L12.3036 12.0586C12.6145 12.9718 11.5798 13.7523 10.7871 13.2025L8.19462 11.4046C7.85187 11.1668 7.39758 11.1668 7.05483 11.4046L4.46235 13.2025C3.66969 13.7523 2.63496 12.9718 2.9458 12.0586L3.88528 9.29851C4.02915 8.87584 3.87539 8.40901 3.50851 8.15456L1.09777 6.48263C0.292535 5.92416 0.687722 4.66091 1.66767 4.66091H4.74788C5.17598 4.66091 5.55659 4.3884 5.69454 3.98313L6.67806 1.09367Z" fill="#FFA858" />
+                            </svg>
+
+                            <svg className="product-item__star--empty" width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M6.67806 1.09367C6.98566 0.189974 8.26378 0.189977 8.57138 1.09367L9.5549 3.98313C9.69285 4.3884 10.0735 4.66091 10.5016 4.66091H13.5818C14.5617 4.66091 14.9569 5.92416 14.1517 6.48263L11.7409 8.15456C11.3741 8.40901 11.2203 8.87584 11.3642 9.29851L12.3036 12.0586C12.6145 12.9718 11.5798 13.7523 10.7871 13.2025L8.19462 11.4046C7.85187 11.1668 7.39758 11.1668 7.05483 11.4046L4.46235 13.2025C3.66969 13.7523 2.63496 12.9718 2.9458 12.0586L3.88528 9.29851C4.02915 8.87584 3.87539 8.40901 3.50851 8.15456L1.09777 6.48263C0.292535 5.92416 0.687722 4.66091 1.66767 4.66091H4.74788C5.17598 4.66091 5.55659 4.3884 5.69454 3.98313L6.67806 1.09367Z" fill="#FFA858" />
+                            </svg>
+                        </div>
+
+                        <div className="product-item__price-wrap">
+                            <span className="product-card__price--sale"><s>$20.00</s></span>
+
+                            <span className="product-card__price">$13.00</span>
+                        </div>
+
+                        <p className="product-item__description text">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+
+                        <form className="product-item__form">
+                            <label htmlFor="productItemInputCount" className="product-item__label product-item__label--count">
+                                Quantity:
+
+                                <input type="number" className="product-item__input product-item__input--count" id="productItemInputCount" />
+                            </label>
+
+                            <button type="submit" className="product-item__input product-item__input--submit button button--blue">
+                                Add To Cart
+
+                                <svg width="20" height="19" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="10" cy="9.5" r="9.5" fill="#335B6B" />
+                                    <path className="button__svg-arrow" d="M9.97641 6.12891L13.371 9.19342L9.97641 12.2579M12.8995 9.19342H6.01611" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="product-item__row product-item__row--2 row">
+                    <div className="product-item__column product-item__column--3 column">
+                        <div className="product-item__switch-wrap">
+                            <button className="product-item__button product-item__button--description button">Product Description</button>
+                            <button className="product-item__button product-item__button--additional button">Additional Info</button>
+                        </div>
+
+                        <p className="product-item__info text">
+                            Welcome to the world of natural and organic. Here you can discover the bounty of nature. We have grown on the principles of health, ecology, and care. We aim to give our customers a healthy chemical-free meal for perfect nutrition. It offers about 8–10% carbs. Simple sugars — such as glucose and fructose — make up 70% and 80% of the carbs in raw.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProductItemSection
--- frontend/src/pages/ProductPage/ProductPage.tsx
+++ frontend/src/pages/ProductPage/ProductPage.tsx
@@ -1,11 +1,15 @@
 import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
+import SubIntroBlock from "../../components/SubIntroBlock/SubIntroBlock"
+import ShopSingleBackgroundImage from "../../images/images/background-shop-single.png"
+import ProductItemSection from "../../components/ProductItemSection/ProductItemSection"
 
 function ProductPage() {
     useSetCurrentPage("product")
 
     return (
         <>
-            <h1>ProductPage</h1>
+            <SubIntroBlock title="Shop Single" backgroundImageURL={ShopSingleBackgroundImage} />
+            <ProductItemSection />
         </>
     )
 }
