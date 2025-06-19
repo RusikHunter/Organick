7bda20b39382ad234f055787d4fba224a72d4e17
dmfshove <funnymoments610@gmail.com>
2025-06-19 14:13:20 +0200

    feat(team): write HTML markup for TeamSection component

---
+++ frontend/src/components/TeamSection/TeamSection.tsx
@@ -0,0 +1,25 @@
+import "./TeamSection.scss"
+
+function TeamSection() {
+    return (
+        <section className="team">
+            <div className="team__inner container">
+                <div className="team__row team__row--1 row">
+                    <div className="team__column column">
+                        <span className="team__promo promo">Team</span>
+
+                        <h2 className="team__title h2">Our Organic Experts</h2>
+
+                        <p className="team__description text">Simply dummy text of the printing and typesetting industry. Lorem had ceased to been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+                    </div>
+                </div>
+
+                <div className="team__row team__row--2 row">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default TeamSection
