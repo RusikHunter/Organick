305d0251175482651b73a751e809824e50fec1cc
dmfshove <funnymoments610@gmail.com>
2025-06-19 15:46:01 +0200

    feat(recent): write HTML markup for RecentSection component

---
+++ frontend/src/components/RecentSection/RecentSection.tsx
@@ -0,0 +1,15 @@
+import "./RecentSection.scss"
+
+function RecentSection() {
+    return (
+        <section className="recent">
+            <div className="recent__inner container">
+                <div className="recent__row row">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default RecentSection
--- frontend/src/pages/BlogPage/BlogPage.tsx
+++ frontend/src/pages/BlogPage/BlogPage.tsx
@@ -1,11 +1,14 @@
 import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
+import SubIntroBlock from "../../components/SubIntroBlock/SubIntroBlock"
+import RecentBackgroundImage from "../../images/images/background-recent.png"
 
 function BlogPage() {
     useSetCurrentPage("blog")
 
     return (
         <>
-            <h1>BlogPage</h1>
+            <SubIntroBlock title="Recent News" backgroundImageURL={RecentBackgroundImage} />
+
         </>
     )
 }
