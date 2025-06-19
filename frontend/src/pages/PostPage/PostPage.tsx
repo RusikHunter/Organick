254da8b7972caacef1f3524e78d31bc560a53825
dmfshove <funnymoments610@gmail.com>
2025-06-19 18:01:30 +0200

    feat(postcontent): write HTML markup for PostContentSection component

--- frontend/src/pages/PostPage/PostPage.tsx
+++ frontend/src/pages/PostPage/PostPage.tsx
@@ -2,6 +2,7 @@ import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
 import IntroBlock from "../../components/IntroBlock/IntroBlock"
 import ManNextToTheTreeImage from "../../images/images/man-next-to-tree.png"
 import IntroBlockPost from "../../components/IntroBlockPost/IntroBlockPost"
+import PostContentSection from "../../components/PostContentSection/PostContentSection"
 
 function PostPage() {
     useSetCurrentPage("post")
@@ -9,6 +10,7 @@ function PostPage() {
     return (
         <>
             <IntroBlock content={<IntroBlockPost />} backgroundImageURL={ManNextToTheTreeImage} />
+            <PostContentSection />
         </>
     )
 }
