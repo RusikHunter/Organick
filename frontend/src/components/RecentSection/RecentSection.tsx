09528da0e89b4151a57bbe82485e168f13cc902d
dmfshove <funnymoments610@gmail.com>
2025-06-19 16:28:05 +0200

    feat(news): create RecentSection, NewsWrap and NewsCard components, replace news blocks in MoreNewsSection with NewsWrap component

--- frontend/src/components/RecentSection/RecentSection.tsx
+++ frontend/src/components/RecentSection/RecentSection.tsx
@@ -1,11 +1,12 @@
 import "./RecentSection.scss"
+import NewsWrap from "../NewsWrap/NewsWrap"
 
 function RecentSection() {
     return (
         <section className="recent">
             <div className="recent__inner container">
                 <div className="recent__row row">
-
+                    <NewsWrap defaultNewsCount={8} />
                 </div>
             </div>
         </section>
--- frontend/src/pages/BlogPage/BlogPage.tsx
+++ frontend/src/pages/BlogPage/BlogPage.tsx
@@ -1,6 +1,7 @@
 import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
 import SubIntroBlock from "../../components/SubIntroBlock/SubIntroBlock"
 import RecentBackgroundImage from "../../images/images/background-recent.png"
+import RecentSection from "../../components/RecentSection/RecentSection"
 
 function BlogPage() {
     useSetCurrentPage("blog")
@@ -8,7 +9,7 @@ function BlogPage() {
     return (
         <>
             <SubIntroBlock title="Recent News" backgroundImageURL={RecentBackgroundImage} />
-
+            <RecentSection />
         </>
     )
 }
