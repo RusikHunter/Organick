b11df97abb8ee7f9ecb5664fb263e8ad085663c9
dmfshove <funnymoments610@gmail.com>
2025-06-20 17:11:09 +0200

    styles(utilsintro): write SCSS styles for UtilsIntroBlock component

--- frontend/src/components/UtilsIntroBlock/UtilsIntroBlock.tsx
+++ frontend/src/components/UtilsIntroBlock/UtilsIntroBlock.tsx
@@ -2,12 +2,11 @@ import "./UtilsIntroBlock.scss"
 
 interface UtilsIntroBlockProps {
     title: string
-    backgroundImageURL: string
 }
 
-function UtilsIntroBlock({ title, backgroundImageURL }: UtilsIntroBlockProps) {
+function UtilsIntroBlock({ title }: UtilsIntroBlockProps) {
     return (
-        <section className="utils-intro" style={{ backgroundImage: `url(${backgroundImageURL})` }}>
+        <section className="utils-intro">
             <div className="utils-intro__inner container">
                 <div className="utils-intro__row row">
                     <h2 className="utils-intro__title h2">{title}</h2>
--- frontend/src/pages/LicensesPage/LicensesPage.tsx
+++ frontend/src/pages/LicensesPage/LicensesPage.tsx
@@ -1,13 +1,12 @@
 import { useSetCurrentPage } from "../../hooks/useSetCurrentPage"
 import UtilsIntroBlock from "../../components/UtilsIntroBlock/UtilsIntroBlock"
-import UtilPageBackgroundImage from "../../images/images/background-util-page.png"
 
 function LicensesPage() {
     useSetCurrentPage("licenses")
 
     return (
         <>
-            <UtilsIntroBlock title="Licenses" backgroundImageURL={UtilPageBackgroundImage} />
+            <UtilsIntroBlock title="Licenses" />
         </>
     )
 }
