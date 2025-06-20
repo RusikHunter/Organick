f29d83f018e7cd2b50f194e7eb92683c85315a02
dmfshove <funnymoments610@gmail.com>
2025-06-20 17:50:09 +0200

    styles(licenses): write SCSS styles for LicensesSection component

--- frontend/src/components/LicensesSection/LicensesSection.tsx
+++ frontend/src/components/LicensesSection/LicensesSection.tsx
@@ -11,7 +11,7 @@ function LicensesSection() {
                                 <h3 className="licenses__title h3">Icon & Graphics</h3>
                             </div>
 
-                            <div className="lecenses__content-wrap">
+                            <div className="licenses__content-wrap">
                                 <p className="licenses__text text">Icons and Graphics are manually designed by the VictorFlow Templates team. You may download these and edit them to fit your website without asking for permission or providing credit.
                                 </p>
 
@@ -24,16 +24,16 @@ function LicensesSection() {
                                 <h3 className="licenses__title h3">Photography</h3>
                             </div>
 
-                            <div className="lecenses__content-wrap">
+                            <div className="licenses__content-wrap">
                                 <p className="licenses__text text">All images used in the Organick Webflow Template are licensed for free personal and commercial use. If you'd like to use any specific image, you can check the licenses and download the images for free on Unsplash, Pexels‍. And Freepik.</p>
 
                                 <h5 className="licenses__subtitle h5">Unsplash</h5>
 
-                                <p className="licenses__text text">Image 1,  Image 2, Image 3, Image 4, Image 5, Image 6, Image 7, Image 8, Image 9, Image 10, Image 11, Image 12, Image 13, Image 14, Image 15, Image 16, Image 17, Image 18, Image 19, Image 20, Image 21, Image 22, Image 23, Image 24, Image 25</p>
+                                <p className="licenses__text licenses__text--enumeration text">Image 1,  Image 2, Image 3, Image 4, Image 5, Image 6, Image 7, Image 8, Image 9, Image 10, Image 11, Image 12, Image 13, Image 14, Image 15, Image 16, Image 17, Image 18, Image 19, Image 20, Image 21, Image 22, Image 23, Image 24, Image 25</p>
 
                                 <h5 className="licenses__subtitle h5">Pixcel</h5>
 
-                                <p className="licenses__text text">Image 1,  Image 2, Image 3, Image 4, Image 5, Image 6, Image 7, Image 8, Image 9, Image 10, Image 11, Image 12, Image 13, Image 14, Image 15, Image 16, Image 17</p>
+                                <p className="licenses__text licenses__text--enumeration text">Image 1,  Image 2, Image 3, Image 4, Image 5, Image 6, Image 7, Image 8, Image 9, Image 10, Image 11, Image 12, Image 13, Image 14, Image 15, Image 16, Image 17</p>
                             </div>
                         </div>
 
@@ -42,8 +42,8 @@ function LicensesSection() {
                                 <h3 className="licenses__title h3">Photography</h3>
                             </div>
 
-                            <div className="lecenses__content-wrap">
-                                <p className="licenses__text text">Organick template uses free licensed Google Fonts. Please check <span className="licenses__text--bold">Roboto, Yellowtail</span> and <span className="licenses__text--bold">Open Sans.</span></p>
+                            <div className="licenses__content-wrap">
+                                <p className="licenses__text licenses__text--short text">Organick template uses free licensed Google Fonts. Please check <span className="licenses__text--bold">Roboto, Yellowtail</span> and <span className="licenses__text--bold">Open Sans.</span></p>
                             </div>
                         </div>
                     </div>
