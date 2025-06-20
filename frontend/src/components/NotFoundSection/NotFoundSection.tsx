f1d778de104973a75b6f5f8d300310ad71f67d17
dmfshove <funnymoments610@gmail.com>
2025-06-20 19:50:43 +0200

    styles(notfound): write SCSS styles for NotFoundSection component

--- frontend/src/components/NotFoundSection/NotFoundSection.tsx
+++ frontend/src/components/NotFoundSection/NotFoundSection.tsx
@@ -1,4 +1,5 @@
 import "./NotFoundSection.scss"
+import { Link } from "react-router-dom"
 
 function NotFoundSection() {
     return (
@@ -14,14 +15,16 @@ function NotFoundSection() {
                             <p className="not-found__text text">The page you are looking for doesn't exist or has been moved</p>
                         </div>
 
-                        <button className="not-found__button button button--blue">
-                            Go to Homepage
+                        <Link to="/" className="not-found__link" tabIndex={-1}>
+                            <button className="not-found__button button button--blue">
+                                Go to Homepage
 
-                            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="9.5" cy="9.5" r="9.5" fill="currentColor" />
-                                <path className="button__svg-arrow" d="M9.47641 6.12891L12.871 9.19342L9.47641 12.2579M12.3995 9.19342H5.51611" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
+                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="9.5" cy="9.5" r="9.5" fill="currentColor" />
+                                    <path className="button__svg-arrow" d="M9.47641 6.12891L12.871 9.19342L9.47641 12.2579M12.3995 9.19342H5.51611" stroke="white" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
