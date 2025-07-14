0210313c5f8bee46068cb705315e31c048f43b06
dmfshove <funnymoments610@gmail.com>
2025-07-14 20:40:34 +0200

    feat(posts): display posts in the NewsWrap component, and display post content in sections of the PostPage component

--- frontend/src/components/NewsWrap/NewsWrap.tsx
+++ frontend/src/components/NewsWrap/NewsWrap.tsx
@@ -1,16 +1,29 @@
 import "./NewsWrap.scss"
 import NewsCard from "../NewsCard/NewsCard"
 import type { NewsWrapProps } from "../../interfaces/newsWrapProps"
+import { useAppSelector } from "../../hooks/useAppSelector"
 
 function NewsWrap({ defaultNewsCount }: NewsWrapProps) {
-    const newsCount = Array.from({ length: defaultNewsCount }, (_, i) => i)
+    const posts = useAppSelector(state => state.client.posts)
+
+    // if the specified default count is greater than the number of posts
+    defaultNewsCount = defaultNewsCount > posts.length ? posts.length : defaultNewsCount
+
+    const slisedPosts = posts.slice(0, defaultNewsCount)
 
     return (
-        <article className="news-wrap">
-            {newsCount.map(index => (
-                <NewsCard key={index} />
-            ))}
-        </article>
+        <>
+            {posts.length
+                ?
+                <article className="news-wrap">
+                    {slisedPosts.map((post, index) => (
+                        <NewsCard key={index} post={post} />
+                    ))}
+                </article>
+                :
+                <h5 className="news-wrap__text--loading h5">Loading...</h5>
+            }
+        </>
     )
 }
 
