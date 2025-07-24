c3667b7e9d405f95e8743c10152ffae553bc7d6a
dmfshove <funnymoments610@gmail.com>
2025-07-24 19:39:15 +0200

    perf(components): optimize repeated JSX in TeamExpertCard and WhyUsSection components

--- src/components/WhyUsSection/WhyUsSection.tsx
+++ src/components/WhyUsSection/WhyUsSection.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import "./WhyUsSection.scss"
 import GreenVegetablesImage from "@assets/images/decorative/green-vegetables.webp"
 import GreenVegetablesBlurredImage from "@assets/images/decorative/green-vegetables-blurred.webp"
@@ -8,6 +9,12 @@ import PaymentIcon from "@assets/icons/payment.svg"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import "react-lazy-load-image-component/src/effects/blur.css"
 import type { WhyUsFeature } from "@interfaces/whyUsFeature"
+import WhyUsSVGDecoration from "./WhyUsSVGDecoration"
+
+type WhyUsDescriptionListItem = {
+    term: string,
+    definition: string
+}
 
 function WhyUsSection() {
     const features: WhyUsFeature[] = [
@@ -45,6 +52,11 @@ function WhyUsSection() {
         },
     ]
 
+    const descriptionList: WhyUsDescriptionListItem[] = [
+        { term: "100% Natural Product", definition: "Our Biona Organic Granola with Wild Berries boosts immunity and keeps you energized." },
+        { term: "Increases resistance", definition: "Filling, and temptingly healthy, our Biona Organic Granola with Wild Berries is just the thing." }
+    ]
+
     return (
         <section className="whyus">
             <div className="whyus__inner container">
@@ -58,25 +70,16 @@ function WhyUsSection() {
                         <p className="whyus__description text">Our products never pass through middlemen or open markets, guaranteeing top quality for you.</p>
 
                         <dl className="whyus__description-list">
-                            <dt className="whyus__description-term">
-                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="9.5" cy="9.5" r="9.5" fill="#7EB693" />
-                                    <circle cx="9.5" cy="9.5" r="5.5" fill="#ECECEC" />
-                                </svg>
+                            {descriptionList.map((listItem, index) => (
+                                <React.Fragment key={index}>
+                                    <dt className="whyus__description-term">
+                                        <WhyUsSVGDecoration />
 
-                                100% Natural Product
-                            </dt>
-                            <dd className="whyus__description-definition text">Our Biona Organic Granola with Wild Berries boosts immunity and keeps you energized.</dd>
-
-                            <dt className="whyus__description-term">
-                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <circle cx="9.5" cy="9.5" r="9.5" fill="#7EB693" />
-                                    <circle cx="9.5" cy="9.5" r="5.5" fill="#ECECEC" />
-                                </svg>
-
-                                Increases resistance
-                            </dt>
-                            <dd className="whyus__description-definition text">Filling, and temptingly healthy, our Biona Organic Granola with Wild Berries is just the thing</dd>
+                                        {listItem.term}
+                                    </dt>
+                                    <dd className="whyus__description-definition text">{listItem.definition}</dd>
+                                </React.Fragment>
+                            ))}
                         </dl>
                     </div>
 
