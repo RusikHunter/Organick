9c8f58ba6bcdce9d52bd418b2aa966e461d6494f
dmfshove <funnymoments610@gmail.com>
2025-07-27 21:13:04 +0200

    fix(pages): remove blurred images import in page components

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -1,6 +1,5 @@
 import SubIntroBlock from "@components/SubIntroBlock/SubIntroBlock"
 import AboutUsBackgroundImage from "@assets/images/background/background-about-us.webp"
-import AboutUsBackgroundBlurredImage from "@assets/images/background/background-about-us-blurred.webp"
 import CreativeSection from "@components/CreativeSection/CreativeSection"
 import WhyUsSection from "@components/WhyUsSection/WhyUsSection"
 import TeamSection from "@components/TeamSection/TeamSection"
@@ -9,7 +8,7 @@ import OfferSection from "@components/OfferSection/OfferSection"
 function AboutPage() {
     return (
         <>
-            <SubIntroBlock title="About Us" backgroundImageURL={AboutUsBackgroundImage} blurredBackgroundImageURL={AboutUsBackgroundBlurredImage} />
+            <SubIntroBlock title="About Us" backgroundImageURL={AboutUsBackgroundImage} />
             <CreativeSection />
             <WhyUsSection />
             <TeamSection defaultExpertsCount={3} />
--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,13 +1,12 @@
 import SubIntroBlock from "@components/SubIntroBlock/SubIntroBlock"
 import ContactUsBackgroundImage from "@assets/images/background/background-contact-us.webp"
-import ContactUsBackgroundBlurredImage from "@assets/images/background/background-contact-us-blurred.webp"
 import ContactUsSection from "@components/ContactUsSection/ContactUsSection"
 import LocationSection from "@components/LocationSection/LocationSection"
 
 function ContactPage() {
     return (
         <>
-            <SubIntroBlock title="Contact Us" backgroundImageURL={ContactUsBackgroundImage} blurredBackgroundImageURL={ContactUsBackgroundBlurredImage} />
+            <SubIntroBlock title="Contact Us" backgroundImageURL={ContactUsBackgroundImage} />
             <ContactUsSection />
             <LocationSection />
         </>
