957d662902620484d4743634b46a384052806aa7
dmfshove <funnymoments610@gmail.com>
2025-07-27 20:18:01 +0200

    fix(config): change MIN_PRODUCT_COUNT and MAX_PRODUCT_COUNT values

--- src/config/product-settings.ts
+++ src/config/product-settings.ts
@@ -1,4 +1,4 @@
 export const MIN_PRODUCT_COUNT: number = 0
 export const MAX_PRODUCT_COUNT: number = 100
-export const TAXES_VALUE: number = 0
-export const DELIVERY_VALUE: number = 0
+export const TAXES_VALUE: number = 2
+export const DELIVERY_VALUE: number = 5
